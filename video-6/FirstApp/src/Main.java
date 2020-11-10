6ee618ebbf0f178562553aeb9d2b68fe385a7f3d
nishasajnani <sajnaninisha19@gmail.com>
2020-11-09 18:26:49 -0800

    First Java Program

---
+++ video-6/FirstApp/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+     public static void main(String[] a) {
+	// write your code here
+        System.out.print("Welcome to our first app");
+         int d=10,b=5,c=20;
+
+         System.out.print(d + " " + b + " " + c);
+    }
+}
